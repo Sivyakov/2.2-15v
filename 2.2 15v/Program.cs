bf7c577c87f985c8fd3d34241ee9fa2ed97ba83c
сивяков никит <reus-kit@mail.ru>
2021-12-23 14:12:49 +0600

    Добавьте файлы проекта.

---
+++ 2.2 15v/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2._2_15v
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2.2. Начальный уровень
+            try
+            {
+                Console.WriteLine("Введите A(x0;y0):");
+                double x0 = double.Parse(Console.ReadLine());
+                double y0 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите B(x1;y1):");
+                double x1 = double.Parse(Console.ReadLine());
+                double y1 = double.Parse(Console.ReadLine());
+                if (x0 + y0 < x1 + y1) Console.WriteLine("Точка A(x0;y0) наименее удалена от начала кординат");
+                else Console.WriteLine("Точка B(x1;y1) наименее удалена от начала кординат");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
